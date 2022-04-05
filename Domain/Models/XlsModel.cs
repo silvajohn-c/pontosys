71a69d298f755be0e20a9802ae3b4532fc10d8a7
Eitydash <silvajohn.costa@gmail.com>
2022-04-04 21:08:28 -0300

    Change Csv to Xls

---
+++ Domain/Models/XlsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace pontosys.Domain.Models
+{
+    public class XlsModel
+    {
+        public string Nome { get; set; } = "";
+        public string Sobrenome { get; set; } = "";
+        public long Cpf { get; set; }
+        public string Cargo { get; set; } = "";
+        public int CargaHoraria { get; set; }
+        public string ModalidadeContrato { get; set; } = "";
+        public DateTime InicioContrato { get; set; }
+    }
+}
